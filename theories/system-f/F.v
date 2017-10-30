486b99c3602e0f900a9f14dacdf40fb7ce458e3f
Matthew Chan <matt@awakenetworks.com>
2017-10-29 18:29:23 -0700

    f

---
+++ theories/system-f/F.v
@@ -0,0 +1,18 @@
+(* chapter 16 of harper's practical foundations of programming laguages *)
+Module SystemF.
+  (* de bruijn indexed types *)
+  Inductive tau : Type -> Type :=
+  | TVar : nat -> tau nat
+  | Arr  : forall t1 t2, tau t1 -> tau t2 -> tau (t1 -> t2)
+  | All  : forall t, tau nat -> tau t -> tau (nat -> t)
+  .
+
+  Inductive exp : Type -> Type :=
+  | EVar : forall t, t -> exp t
+  | Lam  : forall t1 t2, tau t1 -> exp t1 -> exp t2 -> exp (t1 -> t2)
+  | App  : forall t1 t2, exp (t1 -> t2) -> exp t1 -> exp t2
+  | TLam : forall t, tau nat -> exp t -> exp (nat -> t)
+  | TApp : forall t, tau nat -> exp (nat -> t) -> exp t
+  .
+
+End SystemF.
